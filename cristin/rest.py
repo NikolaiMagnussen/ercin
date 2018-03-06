5c0d1a09eef7d8f732a3489446b9ec24fb1efd40
Nikolai Magnussen <nikolai@magnussen.tf>
2018-03-06 15:58:58 +0100

    API skeleton for both Person and Result is finished

--- cristin/rest.py
+++ cristin/rest.py
@@ -1,2 +1,61 @@
-def kake():
-    print(f"kake from {__name__}")
+import requests
+import ws
+
+
+class Person():
+    URL = "https://api.cristin.no/v2/persons"
+
+    def __init__(self, id=None, **kwargs):
+        if id is not None:
+            res = requests.get(f"{self.URL}/{id}")
+            if res.status_code == 200:
+                self.__attributes = res.json()
+            else:
+                raise LookupError
+        elif id is None and kwargs != {}:
+            self.__attributes = kwargs
+        else:
+            raise TypeError
+
+    def get_results(self):
+        return ws.get_results_by_person_id(self.cristin_person_id)
+
+    @property
+    def first_name(self):
+        return self.__attributes['first_name']
+
+    @property
+    def surname(self):
+        return self.__attributes['surname']
+
+    @property
+    def private_email(self):
+        return self.__attributes['private_email']
+
+    @property
+    def tel(self):
+        return self.__attributes['tel']
+
+    @property
+    def cristin_person_id(self):
+        return self.__attributes['cristin_person_id']
+
+    @property
+    def identified_cristin_person(self):
+        return self.__attributes['identified_cristin_person']
+
+    @property
+    def date_of_birth(self):
+        return self.__attributes['date_of_birth']
+
+    @property
+    def picture_url(self):
+        return self.__attributes['picture_url']
+
+    @property
+    def cristin_profile_url(self):
+        return self.__attributes['cristin_profile_url']
+
+    @property
+    def affiliations(self):
+        return self.__attributes['affiliations']
--- cristin/ws.py
+++ cristin/ws.py
@@ -1,2 +1,58 @@
-def kake():
-    print(f"kake from {__name__}")
+import requests
+
+def get_results_by_person_id(lopenr):
+    res = requests.get(f"https://cristin.no/ws/hentVarbeiderPerson"
+                       f"?lopenr={person_id}&format=json")
+    if res.status_code != 200:
+        return []
+
+    return [x['fellesdata'] for x in res.json()['forskningsresultat']]
+
+
+class Result():
+    def __init__(self, **kwargs):
+        self.__attributes = kwargs
+
+    def get_collaborators(self):
+        authors = self.person
+
+
+    @property
+    def id(self):
+        return self.__attributes['id']
+
+    @property
+    def eier(self):
+        return self.__attributes['eier']
+
+    @property
+    def registrert(self):
+        return self.__attributes['registrert']
+
+    @property
+    def endret(self):
+        return self.__attributes['endret']
+
+    @property
+    def kategori(self):
+        return self.__attributes['kategori']
+
+    @property
+    def person(self):
+        return self.__attributes['person']
+
+    @property
+    def sprak(self):
+        return self.__attributes['sprak']
+
+    @property
+    def tittel(self):
+        return self.__attributes['tittel']
+
+    @property
+    def ar(self):
+        return self.__attributes['ar']
+
+    @property
+    def erPublisert(self):
+        return self.__attributes['erPublisert']
